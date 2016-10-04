61f0312d73324bd75a6998b7c14a056302e4ef72
Terrence Plunkett <eightys3v3n+github@gmail.com>
2016-10-04 10:37:06 -0600

    small change i forget

--- source/structures.cpp
+++ source/structures.cpp
@@ -18,7 +18,7 @@ Screen::Screen(sf::Vector2f screenSize)
   sf::Vector2f scrollBarSize(10,50); // the size, in pixels, of the scroll bar (x,y)
   sf::Color scrollBarColor(100,100,100,100); // the colour of the scroll bar (r,g,b,alpha) i think
 
-  background.setPosition(sf::Vector2f(0,0));
+  background.setPosition(sf::Vector2f(0,0)); // see note 412312345
   background.setOrigin(sf::Vector2f(screenSize.x/2,screenSize.y/2));
   background.setSize(screenSize);
 
